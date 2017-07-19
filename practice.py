a77ba52b7c3f764d69d02fcaa6fef120b56bb744
Annie Chu <chuannie4@gmail.com>
2017-07-19 14:37:21 -0700

    cosine curve animated

--- practice.py
+++ practice.py
@@ -10,4 +10,22 @@ fig = plt.figure() #creates figure window
 ax = plt.axes(xlim=(0,2), ylim=(-2,2))
 line, = ax.plot([], [], lw=2)
 
+#initial function for plotting the background of every frame
+def init():
+	line.set_data([], [])
+	return line,
+
+#animation function
+def animate(i): #only takes i as a single parameter ALWAYS i = frame number
+	x = np.linspace(0,2,1000)
+	y = np.cos(2*np.pi* (x-0.01*i))
+	line.set_data(x,y)
+	return line,
+
+anim = animation.FuncAnimation(fig, animate, init_func=init, frames=150, interval=20, blit=True)
+#fig is like void setup in processing
+#animate changes the x and y every frame
+#interval = delay in ms
+#blit tells the animation to only redraw the pieces of the plot that changed
+
 plt.show()

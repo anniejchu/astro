a59a6c7b8916daaa78c59253fd23210253c26167
Annie Chu <chuannie4@gmail.com>
2017-07-19 16:29:21 -0700

    added a box, changed size of sphere

--- vpythontest.py
+++ vpythontest.py
@@ -1,3 +1,4 @@
 from visual import *
 
-sphere(pos = vector(1,0,0), radius = 0.5, color = color.green)
+sphere(pos = vector(-5,0,0), radius = 0.5, color = color.green)
+wallR = box(pos = (6,0,0), size = (0.2, 12, 12), color = color.red)

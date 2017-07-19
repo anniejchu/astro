7067e4062178ff5503682c326786f8d6f2e95e7c
Annie Chu <chuannie4@gmail.com>
2017-07-19 16:52:23 -0700

    move ball

--- vpythontest.py
+++ vpythontest.py
@@ -1,4 +1,12 @@
 from visual import *
 
-sphere(pos = vector(-5,0,0), radius = 0.5, color = color.green)
+ball = sphere(pos = vector(-5,0,0), radius = 0.5, color = color.green)
 wallR = box(pos = (6,0,0), size = (0.2, 12, 12), color = color.red)
+ball.velocity = vector(25, 0, 0)
+deltat = 0.005
+t=0
+while t<3:
+	ball.pos = ball.pos + ball.velocity*deltat
+	t = t+deltat
+
+

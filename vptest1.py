6a46b05ab1a77434e3e205cf20305c3d0bce1e0e
Annie Chu <chuannie4@gmail.com>
2017-07-20 14:10:06 -0700

    created a 'lens'

--- vptest1.py
+++ vptest1.py
@@ -1,4 +1,4 @@
-
+'''
 from visual import *
 r = vector(-3,4,0)
 
@@ -13,3 +13,39 @@ while circle2.x < 10:
 	#circle2.pos = r
 	circle2.x = circle2.x +1
 
+'''
+
+from visual import *
+import numpy as np 
+
+#setup
+scene.width=800
+scene.height=600
+
+#constants
+c = 3.00*(10**8) #m/s
+G = 6.67408*(10**-11) #m^3/kgs^2
+
+#variables
+mL = 5 #mass of lens in solar masses
+xS0 = vector(-5,0,0) #initial location of the star
+dL = 4000 #distance to the lens in pc
+dS = 8000 #distance to the star in pc
+     #proper motion: maybe input the the radial velocity and tangetial velocity
+#muS = pass #diff_angle(v1,v2) to find the angle between 2 vectors
+#muL = pass 
+
+'''
+
+#calculations for angular einsten ring
+mL = mL*(7.3477*10**22) #solar mass to kg
+dL = dL*(3.0857*10**16)
+dS = dL*(3.0857*10**16)
+
+DIdist = (dL**-1)-(dS**-1)
+pt1 = (4*G*mL)/(c**2)
+thetaE = np.sqrt(DIdist*pt1)
+'''
+
+#-----------------------------------------
+LENS = sphere(pos = vector(0,0, dL), radius = 30, color=color.green)

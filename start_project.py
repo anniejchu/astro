c8233f755be58ba486348f066881ab3c5b16128a
Annie Chu <chuannie4@gmail.com>
2017-07-21 19:13:21 -0700

    added text and labels | next step: using pixel position, left align, change colors to numerical values

--- start_project.py
+++ start_project.py
@@ -1,12 +1,18 @@
 
 from visual import *
 import numpy as np
+#setup
+scene.width = 1000
+scene.height = 500
+scene.autoscale = False
+
 
 #random variables
-origin = vector(0,0,0)
-#setup
-scene.width = 200
-scene.height = 100
+width = -40
+height = 180
+origin = (0,0,0)
+textsize = 12
+
 
 #variables 
 imL = 5 #solar mass MUST CONVERT
@@ -19,8 +25,8 @@ dL = idL * (30856775714409184)
 dS = idS * (30856775714409184)
 G = 6.67 * 10**-11
 c = 3.0 * 10**8
-radtomas = 206264806.247
-mtopc = 30856776000000000
+radtomas = 206265000
+mtopc = 3.0856776000000000 * 10**16
 
 
 #calculating einstein radius 
@@ -32,17 +38,18 @@ thetaE1 = thetaE * radtomas #in mas
 degreeE = (thetaE * 180)/np.pi # radians to degrees
 eradiusm = np.tan(degreeE)*dL #meters
 eradiuspc = eradiusm/mtopc
-eradiuspc_adjusted = eradiuspc*500000
+eradiuspc_adjusted = eradiuspc*120000
 
 #lens positioning
 lPos = vector(0, 0, -idL)
-LENS = sphere(pos = lPos, radius = 100, color=color.blue)
-ER = ring(pos = lPos, radius = eradiuspc_adjusted, axis = (0,0,1), thickness=50, color= color.white)
+LENS = sphere(pos = lPos, radius = 50, color=color.blue)
+ER = ring(pos = lPos, radius = eradiuspc_adjusted, axis = (0,0,1), thickness=15, color= color.white)
 
 #source positioning 
 sPos = vector(-3000.0, 200.0, -idS)
-STAR = sphere(pos=sPos, radius = 100, color = color.yellow)
+STAR = sphere(pos=sPos, radius = 50, color = color.yellow)
 STAR.velocity = vector(250, 0, 0)
+
 
 #deltaT = speed , xlimit = 4000 
 '''
@@ -56,10 +63,23 @@ def movingsource(t, deltaT, timelimit):
 	#REMEMBER THAT THE EINSTEIN CROSSING TIME IS JUST HALF OF THE GRAPH
 	while t < timelimit:
 		STAR.pos = STAR.pos + STAR.velocity*deltaT
+		slabel.pos = slabel.pos + STAR.velocity*deltaT
 		t = t+deltaT
 		rate(100)
 
-movingsource(-20, 0.05, 50)
+#--------------DISPLAY SHIT---------------------------------------
+
+lmasslabel = label(pixel_pos = vector(0,20,0), text = 'lens mass: '+ str(imL) +' solar masses', height = textsize, color = color.white, line = False, align = 'left')
+ldistancelabel = label(pos = STAR.pos, text = 'distance to lens: '+str(idL)+' parsecs', xoffset = width,yoffset= height-25, height = textsize, color = color.white, line = False, align = 'left')
+sdistancelabel = label(pos = STAR.pos, text = 'distance to source: '+str(idS)+ ' parsecs', xoffset = width, yoffset = height-50, height = textsize, color = color.white, line = False, align = 'left')
+erlabel1 = label(pos = STAR.pos, text = 'einstein radius (angular): '+str(thetaE1)+ ' MAS', xoffset = width, yoffset = height-325, height = textsize, color = color.white, line = False, align = 'left')
+erlabel2 = label(pos = STAR.pos, text = '(distance): '+str(eradiuspc)+ ' parsecs', xoffset = width, yoffset = height-350, height = textsize, color = color.white, line = False, align = 'left')
+llabel = label(pos = LENS.pos, text = 'LENS', yoffset=5, height = textsize, color = color.white, line = False)
+slabel = label(pos = STAR.pos, text = 'SOURCE', yoffset=5, height = textsize, color = color.white, line = False)
+erlabel = label(pos = ER.pos, text = 'EINSTEIN RADIUS', yoffset = slabel.yoffset+60, height = textsize-2, color = color.white, line = False)
+
+
+
 
 #light curve info ASK NIJAID WHAT THIS ANGLE IS
 	#radians
@@ -84,3 +104,5 @@ u1 = u *radtomas
 amp1 = ((u1**2)+2)/(u1*(np.sqrt((u1**2)+4)))
 
 
+
+movingsource(-20, 0.05, 50)

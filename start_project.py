a234f91dd2f0763c108af1574be1268373a278ff
Annie Chu <chuannie4@gmail.com>
2017-07-24 10:28:12 -0700

    added observer, put the equations into the while loop, visual text display is still weird

--- start_project.py
+++ start_project.py
@@ -2,20 +2,31 @@
 from visual import *
 import numpy as np
 #setup
-scene.width = 1000
-scene.height = 500
+scene = display(title = 'microlensing', width = 1000, height = 500, background = color.black)
+
 scene.autoscale = False
 
 
 #random variables
-width = -40
-height = 180
-origin = (0,0,0)
+width = -300
+height = 200
+origin = vector(0,0,0)
 textsize = 12
 
+#color variables 
+red = (1,0,0)
+green = (0,1,0)
+blue = (0,0,1)
+yellow = (1,1,0)
+orange = (1,0.5, 0)
+cyan = (0,1,1)
+magenta = (1,0,1)
+black = (0,0,0)
+white = (1,1,1)
+opacity = 0.5
 
-#variables 
-imL = 5 #solar mass MUST CONVERT
+#initial input variables 
+imL = 5 #solar mass 
 idL = 4000 #pc
 idS = 8000 #pc
 
@@ -40,51 +51,39 @@ eradiusm = np.tan(degreeE)*dL #meters
 eradiuspc = eradiusm/mtopc
 eradiuspc_adjusted = eradiuspc*120000
 
+
+#observers
+OBPos = vector(0,0,1)
+OB = sphere(pos = OBPos, radius = 1, color = white)
+
 #lens positioning
-lPos = vector(0, 0, -idL)
-LENS = sphere(pos = lPos, radius = 50, color=color.blue)
-ER = ring(pos = lPos, radius = eradiuspc_adjusted, axis = (0,0,1), thickness=15, color= color.white)
+lPos = vector(0, 0, -idL)+OBPos
+LENS = sphere(pos = lPos, radius = 50, color=blue)
+ER = ring(pos = lPos, radius = eradiuspc_adjusted, axis = (0,0,1), thickness=15, color= white)
 
 #source positioning 
-sPos = vector(-3000.0, 200.0, -idS)
-STAR = sphere(pos=sPos, radius = 50, color = color.yellow)
+sPos = vector(-3000.0, 200.0, -idS)+OBPos
+STAR = sphere(pos=sPos, radius = 50, color = yellow)
 STAR.velocity = vector(250, 0, 0)
 
 
-#deltaT = speed , xlimit = 4000 
-'''
-def movingsource(deltaT, xlimit):
-	while STAR.pos.x < xlimit: 
-		STAR.pos = STAR.pos + STAR.velocity*deltaT
-		rate(100)
-'''
-def movingsource(t, deltaT, timelimit):
-	#FIND EINSTEIN CROSSING TIME, MAKE IT RELEVANT TO THIS FUNCTION
-	#REMEMBER THAT THE EINSTEIN CROSSING TIME IS JUST HALF OF THE GRAPH
-	while t < timelimit:
-		STAR.pos = STAR.pos + STAR.velocity*deltaT
-		slabel.pos = slabel.pos + STAR.velocity*deltaT
-		t = t+deltaT
-		rate(100)
-
 #--------------DISPLAY SHIT---------------------------------------
 
-lmasslabel = label(pixel_pos = vector(0,20,0), text = 'lens mass: '+ str(imL) +' solar masses', height = textsize, color = color.white, line = False, align = 'left')
-ldistancelabel = label(pos = STAR.pos, text = 'distance to lens: '+str(idL)+' parsecs', xoffset = width,yoffset= height-25, height = textsize, color = color.white, line = False, align = 'left')
-sdistancelabel = label(pos = STAR.pos, text = 'distance to source: '+str(idS)+ ' parsecs', xoffset = width, yoffset = height-50, height = textsize, color = color.white, line = False, align = 'left')
-erlabel1 = label(pos = STAR.pos, text = 'einstein radius (angular): '+str(thetaE1)+ ' MAS', xoffset = width, yoffset = height-325, height = textsize, color = color.white, line = False, align = 'left')
-erlabel2 = label(pos = STAR.pos, text = '(distance): '+str(eradiuspc)+ ' parsecs', xoffset = width, yoffset = height-350, height = textsize, color = color.white, line = False, align = 'left')
-llabel = label(pos = LENS.pos, text = 'LENS', yoffset=5, height = textsize, color = color.white, line = False)
-slabel = label(pos = STAR.pos, text = 'SOURCE', yoffset=5, height = textsize, color = color.white, line = False)
-erlabel = label(pos = ER.pos, text = 'EINSTEIN RADIUS', yoffset = slabel.yoffset+60, height = textsize-2, color = color.white, line = False)
+lmasslabel = label(pos = LENS.pos, text = 'lens mass: '+ str(imL) +' solar masses', xoffset = width, yoffset = height, height = textsize, color = white, line = False, align = 'right')
+ldistancelabel = label(pos = LENS.pos, text = 'distance to lens: '+str(idL)+' parsecs', xoffset = width+120,yoffset= height, height = textsize, color = white, line = False, align = 'right')
+sdistancelabel = label(pos = LENS.pos, text = 'distance to source: '+str(idS)+ ' parsecs', xoffset = width+350, yoffset = height, height = textsize, color = white, line = False, align = 'right')
+erlabel1 = label(pos = LENS.pos, text = 'einstein radius (angular): '+str(thetaE1)+ ' MAS', xoffset = width+501, yoffset = height, height = textsize, color =white, line = False, align = 'right')
+#erlabel2 = label(pos = LENS.pos, text = '(distance): '+str(eradiuspc)+ ' parsecs', xoffset = width+700, yoffset = height, height = textsize, color = white, line = False, align = 'right')
+llabel = label(pos = LENS.pos, text = 'LENS', yoffset=5, height = textsize, color =white, line = False)
+slabel = label(pos = STAR.pos, text = 'SOURCE', yoffset=5, height = textsize, color =white, line = False)
+erlabel = label(pos = ER.pos, text = 'EINSTEIN RADIUS', yoffset = slabel.yoffset+60, height = textsize-2, color = white, line = False)
 
-
-
+'''
 
 #light curve info ASK NIJAID WHAT THIS ANGLE IS
 	#radians
 	#BE ABLE TO USE THIS ANGLE TO CREATE A VECTOR USE TRIG????
-thetaS = diff_angle(STAR.pos, origin) 
+thetaS = diff_angle(STAR.pos, OB.pos) 
 thetaPOS = (thetaS + np.sqrt((thetaS**2)+(4*thetaE)))/2
 thetaNEG = (thetaS - np.sqrt((thetaS**2)+(4*thetaE)))/2
 
@@ -102,7 +101,46 @@ amp = ((u**2)+2)/(u*(np.sqrt((u**2)+4)))
 	#mas
 u1 = u *radtomas
 amp1 = ((u1**2)+2)/(u1*(np.sqrt((u1**2)+4)))
+'''
+
+#moving the lems
+def movingsource(t, deltaT, timelimit):
+	#FIND EINSTEIN CROSSING TIME, MAKE IT RELEVANT TO THIS FUNCTION
+	#REMEMBER THAT THE EINSTEIN CROSSING TIME IS JUST HALF OF THE GRAPH
+	while t < timelimit:
+		STAR.pos = STAR.pos + STAR.velocity*deltaT
+		slabel.pos = slabel.pos + STAR.velocity*deltaT
+		#light curve info ASK NIJAID WHAT THIS ANGLE IS
+			#radians
+			#BE ABLE TO USE THIS ANGLE TO CREATE A VECTOR USE TRIG????
+		thetaS = diff_angle(STAR.pos, OB.pos) 
+		thetaPOS = (thetaS + np.sqrt((thetaS**2)+(4*thetaE)))/2
+		thetaNEG = (thetaS - np.sqrt((thetaS**2)+(4*thetaE)))/2
+
+			#mas
+		thetaS1 = thetaS * radtomas
+		thetaPOS1 = (thetaS1 + np.sqrt((thetaS1**2)+(4*thetaE1)))/2
+		thetaNEG1= (thetaS1 - np.sqrt((thetaS1**2)+(4*thetaE1)))/2
+
+
+		#light magnification 
+		#USE THIS TO EITHER CHANGE THE COLOR OR INTENSITY(OPAQUENESS)
+			#radians
+		u = diff_angle(STAR.pos, LENS.pos)
+		amp = ((u**2)+2)/(u*(np.sqrt((u**2)+4)))
+			#mas
+		u1 = u *radtomas
+		amp1 = ((u1**2)+2)/(u1*(np.sqrt((u1**2)+4)))
+
+		print(thetaS)
+		t = t+deltaT
+		rate(100)
 
 
 
-movingsource(-20, 0.05, 50)
+
+
+
+
+
+movingsource(0, 0.05, 20)

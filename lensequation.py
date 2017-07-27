0a704341a1516ffc8626f98061bb4af78fd6c858
Annie Chu <chuannie4@gmail.com>
2017-07-27 16:48:23 -0700

    finessed text display, added slmeet variable

--- lensequation.py
+++ lensequation.py
@@ -5,58 +5,80 @@ ox = -80.0
 oy = 0.0
 oz = -40.0
 opos = vector(ox, oy, oz)
-#observer.pos = (ox, oy)
 
 lx = 0.0
 ly = 0.0
-lz = 40.0
+lz = -40.0
 lpos = vector(lx, ly, lz)
-#lens.pos = (lx, ly)
 
 
 sx = 80.0
 sy = 0.0
 sz = 40.0
 spos = vector(sx, sy, sz)
-#source.pos = (sx, sy)
+
+slmeetpos = vector(lx, ly, sz)
 
 ds = sx-ox
 dl = lx-ox
 dls = sx-lx
 
-#rt = paths.rectangle(pos=(0,0), height= 200, width = 200)
-#curve(pos=rt.pos)
-ols = points(pos=[spos, lpos, opos], size = 10, color = color.red)
+ols = points(pos=[spos, lpos, opos, slmeetpos], size = 10, color = color.red)
 
-
+#LINE FROM OBSERVER TO SOURCE
 osline = paths.line(start=spos, end =opos, np = 20)
 curve(pos=osline.pos)
-olline = paths.line(start=opos, end=lpos, np=20)
-curve(pos=olline.pos)
 
+#LINE FROM OBSERVER TO POINT WHERE SOURCE(parallel) AND LENS(perpendicular) MEET 
+slmeetline = paths.line(start=opos, end=slmeetpos, np=20)
+curve(pos=slmeetline.pos)
+
+#LINE FROM OBSERVER TO SOURCE X AXIS
 dsline = paths.line(start=opos, end=(sx, oy, oz), np = 20)
 curve(pos=dsline.pos)
-dlline = paths.line(start=(ox, oy, oz-4), end=(lx-1, oy, oz-4), np=20)
-curve(pos=dlline.pos)
-dlsline = paths.line(start=(lx+1, oy, oz-4), end=(sx, oy, oz-4), np=20)
-curve(pos=dlsline.pos)
 
+#LINE FROM X AXIS TO SOURCE
 sperpline = paths.line(start=spos, end=(sx, oy, oz), np=20)
 curve(pos=sperpline.pos)
-lperpline = paths.line(start=lpos, end=(lx, oy, oz), np = 50)
-curve(pos=lperpline.pos)
 
-slline = paths.line(start=lpos, end=spos)
-curve(pos=slline.pos)
+#LINE FROM LENS TO SLMEET POINT (aka lens perp line)
+slmeetlline = paths.line(start=lpos, end=slmeetpos, np = 50)
+curve(pos=slmeetlline.pos)
 
+#LINE FROM SLMEET POINT TO SOURCE (parallel)
+slmeetsline = paths.line(start=slmeetpos, end=spos)
+curve(pos=slmeetsline.pos)
+
+#IMAGE STUFF
 ok = lz-oz
-
 theta = atan2(ok,dl)
-psz = (tan(theta))*(ds)
-pspos = vector(sx, sy, psz-lz)
-ps = points(pos=pspos, size = 10, color = color.cyan)
-opsline = paths.line(start=opos, end=pspos, np = 20)
+iz = (tan(theta))*(ds)
+ipos = vector(sx, sy, iz-sz)
+#IMAGE LINE (FROM SLMEET TO IMAGE)
+ps = points(pos=ipos, size = 10, color = color.cyan)
+opsline = paths.line(start=slmeetpos, end=ipos, np = 20)
 curve(pos=opsline.pos)
 
-psperpline = paths.line(start=pspos, end=spos)
-curve(pos=psperpline.pos)
+iperpline = paths.line(start=ipos, end=spos)
+curve(pos=iperpline.pos)
+
+#LABEL LINES
+dsline = paths.line(start=(ox, oy, oz-20), end=(sx, oy, oz-20), np=20)
+curve(pos=dsline.pos)
+dlline = paths.line(start=(ox, oy, oz-10), end=(lx-1, oy, oz-10), np=20)
+curve(pos=dlline.pos)
+dlsline = paths.line(start=(lx+1, oy, oz-10), end=(sx, oy, oz-10), np=20)
+curve(pos=dlsline.pos)
+
+
+#LABELS
+lenslabel = label(pos = lpos, text = 'lens', yoffset = -2, height = 10, color = color.white, line = False)
+sourcelabel = label(pos = spos, text='source', yoffset = -2, height = 10, color = color.white, line = False)
+observerlabel = label(pos = opos, text='observer', yoffset = -2, height = 10, color = color.white, line = False)
+imagelabel = label(pos = ipos, text='image', yoffset = -2, height = 10, color = color.white, line = False)
+slmeetlabel = label(pos= slmeetpos, text= 'sl meet', yoffset= -2, height = 10, color = color.white, line = False)
+
+dlslabel = label(pos = ((sx+lx)*.5, oy, oz-15), text = 'DLS: '+ str(dls) +' pc', color = color.white, height = 8, line = False)
+dllabel = label(pos = ((ox+lx)*.5, oy, oz-15), text = 'DL: '+ str(dl) +' pc', color = color.white,height = 8, line = False)
+dslabel = label(pos = ((ox+sx)*.5, oy, oz-25), text = 'DS: '+ str(ds) +' pc', color = color.white, height = 8,line = False)
+

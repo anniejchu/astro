47264e17d3c443235d6248eef9879389680edbfa
Annie Chu <chuannie4@gmail.com>
2017-07-31 15:36:20 -0700

    added other stars

--- blip.py
+++ blip.py
@@ -6,18 +6,24 @@ scene.height = 600
 scene.autoscale = False
 scene.range = 100
 
-def stargrid(xdiff, ydiff): 
-	for x in range(-40, 40, xdiff):
-		for y in range(-40, 40, ydiff):
-			sphere(pos=(x,y,0), radius = 5, color = color.white, opacity = 0.3)
-
-
 
 
 def lensmoving():
+	rx = []
+	ry = []
+	opacity = []
+	radius=  []
+	for x in range(0, 7):
+		rx.append(random.randint(-90,90))
+		ry.append(random.randint(-90,90))
+		opacity.append(random.uniform(0.2, 0.8))
+		radius.append(random.uniform(1,2))
+		sphere(pos=(rx[x],ry[x],0), radius = radius[x], color = color.white, opacity = opacity[x])
+
 	sx = random.randint(-50,50)
 	sy = random.randint(-50,50)
 	source = sphere(pos=(sx,sy,0), radius = 1.5, color = color.white, opacity = 0.1)
+
 
 	lx = -90
 	ly = -90
@@ -26,7 +32,6 @@ def lensmoving():
 
 	movex = (sx-lx)/50
 	movey = (sy-ly)/50
-	#tr = int(np.sqrt((sx-lx)**2+(sy-ly)**2))
 	
 	while lens.x < 100 and lens.y > -100 and lens.x > -100 and lens.y < 100:
 		lens.x = movex + lens.x
@@ -36,32 +41,9 @@ def lensmoving():
 			source.opacity = source.opacity * 1.5
 		elif lens.x-source.x > 0 and lens.x-source.x < 10:
 			source.opacity = source.opacity / 1.5
-		#elif lens.x-source.x == 0:
-		#	source.opacity = 1
 
 
 		rate(10)
-
-
-def dist():
-	numpy.sqrt((sx-lx)**2+(sy-ly)**2)
-
-	tr = np.absolute(sx-lx)
-	t = np.arange(-tr, tr)
-
-	timelab = label(pos = (-50, 50, 0), text = 1, line = False)
-
-
-	for time in t:
-		lens.x = movex + lens.x
-		lens.y = movey + lens.y
-		timelab.text = str(time)
-
-		if time < 0 and time > -20:
-			source.opacity = source.opacity + 0.05
-		elif time > 0 and time < 20:
-			source.opacity = source.opacity - 0.05
-
 
 lensmoving()
 #stargrid(100,120)
